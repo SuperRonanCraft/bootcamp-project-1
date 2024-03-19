98a04817676ece9faa0cdaa67a23465e2f6f2bf3
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-03-19 12:26:03 -0400

    Selectin inputText and submit button using dom

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,12 @@
 // DEPENDENCIES================
 
+// Heading
 const h1El = document.querySelector('h1');
 
 h1El.textContent = 'MovieMania';
+
+const textInputEl = document.querySelector('#text-input');
+const sumbitButton = document.querySelector('#submitButton');
 
 //DATA===================
 
