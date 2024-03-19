686a2ae64b106bb211deb39d3aea0e7ca69fb282
Nahid Ahmed <npahmed92@gmail.com>
2024-03-19 12:24:46 -0400

    created function to handle form submission

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,6 +5,17 @@ const textInput = document.querySelector('#text-input');
 // DATA ==============================================
 
 // FUNCTIONS =========================================
+function handleFormSubmit(event) {
+  event.preventDefault();
+  // insert file source here
+  const src = 'results.html';
+
+  // assign value from
+  location.assign(`${src}?s=${textInput.value}`);
+
+  // reset input field
+  textInput.value = '';
+}
 
 // USER INTERACTIONS =================================
 
