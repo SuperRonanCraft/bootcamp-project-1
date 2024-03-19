6ce4274fa6cccff2e3044c6c0de80462cb303d26
RonanCraft <SuperRonanCraft@gmail.com>
2024-03-19 10:43:09 -0400

    url fetch and response

--- assets/js/ombdapi.js
+++ assets/js/ombdapi.js
@@ -6,6 +6,11 @@ const apiKey = "b0cdfcde";
 //Functions
 function fetchSearch(movieTitle) {
   const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${movieTitle}&type=movie`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {});
 }
 
 //User Interactions
