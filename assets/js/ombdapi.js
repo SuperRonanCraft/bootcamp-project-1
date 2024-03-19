883f0f2e68a8e82ddad99b5fa74e796272e20dcb
RonanCraft <SuperRonanCraft@gmail.com>
2024-03-19 10:41:47 -0400

    fetch url string literal

--- assets/js/ombdapi.js
+++ assets/js/ombdapi.js
@@ -4,5 +4,8 @@ const apiKey = "b0cdfcde";
 //Data
 
 //Functions
+function fetchSearch(movieTitle) {
+  const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${movieTitle}&type=movie`;
+}
 
 //User Interactions
