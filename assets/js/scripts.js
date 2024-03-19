44f3c2c9c618bc6abc353297030676b62a4e3200
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-03-19 12:20:51 -0400

    Reorganizing script.js file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,11 @@
+// DEPENDENCIES================
+
 const h1El = document.querySelector('h1');
 
 h1El.textContent = 'MovieMania';
+
+//DATA===================
+
+// Functions================
+
+// UserInteractions===================
